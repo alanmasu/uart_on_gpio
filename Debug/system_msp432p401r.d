42dfd1d1bec714975ddedbcd3c00b16ea1c38926
alanmasu <alan.masutti@gmail.com>
2023-12-28 10:15:17 +0100

    Initial commit

---
+++ Debug/system_msp432p401r.d
@@ -0,0 +1,66 @@
+# FIXED
+
+system_msp432p401r.obj: ../system_msp432p401r.c
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+system_msp432p401r.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h
+system_msp432p401r.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+
+../system_msp432p401r.c:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
---
+++ Debug/uart_loopback_24mhz_brclk.d
@@ -0,0 +1,159 @@
+# FIXED
+
+uart_loopback_24mhz_brclk.obj: ../uart_loopback_24mhz_brclk.c
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/driverlib.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/adc14.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/aes256.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/comp_e.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cpu.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/crc32.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cs.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/dma.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/interrupt.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/eusci.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/fpu.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/gpio.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/i2c.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/mpu.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pcm.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pss.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ref_a.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/reset.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom_map.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rtc_c.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/spi.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/systick.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer32.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/uart.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/wdt_a.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/sysctl.h
+uart_loopback_24mhz_brclk.obj: E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/flash.h
+
+../uart_loopback_24mhz_brclk.c:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/driverlib.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/adc14.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+E:/ti/css/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdbool.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/aes256.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/comp_e.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cpu.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/crc32.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/cs.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/dma.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/interrupt.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/eusci.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/fpu.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/gpio.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/i2c.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/mpu.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pcm.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pmap.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/pss.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/ref_a.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/reset.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rom_map.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/rtc_c.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/spi.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/systick.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer32.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/timer_a.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/uart.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/wdt_a.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/sysctl.h:
+
+E:/ti/css/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/driverlib/flash.h:
+
